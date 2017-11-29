4d79e430c45fb39fd2eb72db8a2cf9e0285fd94a
Robin Miller <rmillerpdx@gmail.com>
2017-11-29 14:12:36 -0800

    travis

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Books from './Books';
+import AddBook from './AddBook';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
