20aaba4a6de33b59c82ad86196d60a0d9c344f4a
Robin Miller <rmillerpdx@gmail.com>
2017-11-29 13:59:25 -0800

    state updates properly, does not display to DOM

---
+++ src/Addbook.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class AddBook extends Component {
+
+  render() {
+
+    const { addBook } = this.props;
+
+    return(
+      <div>
+        <form onSubmit={event => {
+          event.preventDefault();
+          const { elements } = event.target;
+          const newBook = {
+            title: elements.title.value,
+            author: elements.author.value,
+          };
+          elements.title.value = elements.author.value = '';
+          return addBook(newBook);
+        }}>
+          <input name="title" placeholder="Title" />
+          <input name="author" placeholder="Author" />
+          <button type="submit">Add Image</button>
+        </form>
+      </div>
+    );
+
+  }
+}
