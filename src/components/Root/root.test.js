b2b898a683c329979ea8df8261fe4b23a9b27525
Robin Miller <rmillerpdx@gmail.com>
2017-12-03 12:37:54 -0800

    snapshot test

--- src/components/Root/root.test.js
+++ src/components/Root/root.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import Root from './Root'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+
+describe('root', () => {
+  
+  it('should match snapshots', () => {
+    const wrapper = shallow(<Root/>)
+
+    expect(wrapper).toMatchSnapshot()
+  })
 })
